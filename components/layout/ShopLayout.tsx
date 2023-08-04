ec791d3cc8182635c6a969cff6fead860cf81895
Daniela Estrada <daniela.es.ib@gmail.com>
2023-08-03 23:08:28 -0600

    create porject next js and material ui e commerce

---
+++ components/layout/ShopLayout.tsx
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import { FC } from 'react'
+
+interface Props {
+  children: any
+  title: string
+  pageDescription: string
+  imageFullUrl?: string
+}
+
+export const ShopLayout: FC<Props> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={pageDescription} />
+				{/* Sirve para redes sociales cuando compartes un link */}
+        <meta name='og:title' content={title} />
+        <meta name='og:description' content={pageDescription} />
+        {imageFullUrl && <meta name='og:image' content={imageFullUrl} />}
+      </Head>
+
+      <nav>{/* TODO: NAVBAR */}</nav>
+
+      {/* TODO: SIDEBAR */}
+
+      <main
+        style={{
+          margin: '80px auto',
+          maxWidth: '1440px',
+          padding: '0px 30px',
+        }}
+      >
+        {children}
+      </main>
+
+      {/* Footer */}
+      <footer>{/* TODO: FOOTER */}</footer>
+    </>
+  )
+}
