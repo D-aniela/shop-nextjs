39a79c446ea62d24ad7ef310e98655c4397cb27d
Daniela Estrada <daniela.es.ib@gmail.com>
2023-08-05 17:10:41 -0600

    add sidemenu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@mui/material'
+
 import { ShopLayout } from '@/components/layout'
-import { Typography } from '@mui/material'
+import { initialData } from '@/database/products'
 
 export default function Home() {
   return (
@@ -13,6 +21,22 @@ export default function Home() {
       <Typography variant='h2' sx={{ marginBottom: 1 }}>
         Todos los productos
       </Typography>
+
+      <Grid container spacing={4}>
+        {initialData.products.map((p) => (
+          <Grid item xs={6} sm={4} key={p.slug}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component='img'
+                  image={`products/${p.images[0]}`}
+                  alt={p.title}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </ShopLayout>
   )
 }
