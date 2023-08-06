23b1e16e9062e152c1f321adc8a4be636323e86b
Daniela Estrada <daniela.es.ib@gmail.com>
2023-08-05 23:47:05 -0600

    add item counter component2

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,5 +1,6 @@
 import { ShopLayout } from '@/components/layout'
 import { ProductSlideShow } from '@/components/products'
+import { ItemCounter } from '@/components/ui'
 import { initialData } from '@/database/products'
 import { Box, Button, Chip, Grid, Typography } from '@mui/material'
 
@@ -26,7 +27,7 @@ const ProductPage = () => {
             {/* Cantidad */}
             <Box sx={{ my: 2 }}>
               <Typography variant='subtitle2'>Cantidad</Typography>
-              {/* ItemCounter */}
+              <ItemCounter />
             </Box>
 
             {/* Agregar al carrito */}
