c4eee6f8848ef129145044b6d91a9585fee8a1b1
Daniela Estrada <daniela.es.ib@gmail.com>
2023-08-06 20:49:22 -0600

    card list component

---
+++ pages/cart/index.tsx
@@ -0,0 +1,46 @@
+import { CarList } from '@/components/cart'
+import { ShopLayout } from '@/components/layout'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+} from '@mui/material'
+
+const CartPage = () => {
+  return (
+    <ShopLayout
+      title='Carrito - 3'
+      pageDescription='Carrito de cmpras de la tienda'
+    >
+      <Typography variant='h1' component='h1'>
+        Carrito
+      </Typography>
+
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <CarList />
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Card className='summary-card'>
+            <CardContent>
+              <Typography variant='h1'>Orden</Typography>
+              <Divider sx={{ my: 1 }} />
+              {/* Order Summary */}
+              <Box sx={{ mt: 3 }}>
+                <Button color='secondary' className='circular-btn' fullWidth>
+                  Checkout
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  )
+}
+
+export default CartPage
