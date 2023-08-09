1e1b9f468230658444cb153c30d275c26b9a33a8
Daniela Estrada <laura.estrada@softtek.com>
2023-08-08 20:09:30 -0600

    create Product model

---
+++ models/Product.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema, model, Model } from 'mongoose'
+import { IProduct } from '@/interfaces'
+
+const productSchema = new Schema(
+  {
+    description: { type: String, required: true },
+    images: [{ type: String, required: true }],
+    inStock: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    sizes: [
+      {
+        type: String,
+        enum: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXX'],
+        message: '{VALUE} no es un tamaño válido',
+      },
+    ],
+    slug: { type: String, required: true, unique: true },
+    tags: [{ type: String }],
+    title: { type: String, required: true },
+    type: [
+      {
+        type: String,
+        enum: ['shirts', 'pants', 'hoodies', 'hats'],
+        message: '{VALUE} no es un tipo válido',
+      },
+    ],
+    gender: {
+      type: String,
+      enum: ['men', 'women', 'kid', 'unisex'],
+      message: '{VALUE} no es un género válido',
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+// TODO: Crear índice de Mongo
+const Product: Model<IProduct> =
+  mongoose.models.Product || model('Product', productSchema)
+
+export default Product
