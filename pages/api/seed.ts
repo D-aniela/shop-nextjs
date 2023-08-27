01b3ad09a886e572576006abce1e07bdac0683f0
Daniela Estrada <daniela.es.ib@gmail.com>
2023-08-27 16:36:23 -0600

    login authentication

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,22 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { db, seedData } from '@/database'
-import { Product } from '@/models'
+import { Product, User } from '@/models'
 
 type Data = {
-	message: string
+  message: string
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-	if(process.env.NODE_ENV === 'production'){
-		return res.status(401).json({ message: 'No tiene acceso a este servicio' })
-	}
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  if (process.env.NODE_ENV === 'production') {
+    return res.status(401).json({ message: 'No tiene acceso a este servicio' })
+  }
 
-	await db.connect()
-	await Product.deleteMany()
-	await Product.insertMany( seedData.initialData.products )
+  await db.connect()
+  await User.deleteMany()
+  await User.insertMany(seedData.initialData.users)
 
-	await db.disconnect()
+  await Product.deleteMany()
+  await Product.insertMany(seedData.initialData.products)
 
-	res.status(200).json({ message: 'Proceso realizado correctamente' })
+  await db.disconnect()
+
+  res.status(200).json({ message: 'Proceso realizado correctamente' })
 }
