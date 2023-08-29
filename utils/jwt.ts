f1e93d194120eef5a169c0c3a5feceadd65e08b3
Daniela Estrada <daniela.es.ib@gmail.com>
2023-08-28 22:28:00 -0600

    json web token by cookies

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -14,3 +14,21 @@ export const signToken = (_id: string, email: string) => {
     { expiresIn: '30d' },
   )
 }
+
+export const isValidToken = (token: string): Promise<string> => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error('JWT_SECRET_SEED is not defined in environment variables')
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
+        if (err) return reject('JWT no es válido')
+        const { _id } = payload as { _id: string }
+        resolve(_id)
+      })
+    } catch (error) {
+      reject('JWT no es válido')
+    }
+  })
+}
