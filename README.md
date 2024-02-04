b3865817262f4809ebe1a555bdece9f57f6d3ec2
Laura Daniela Estrada Ibarra <daniela.es.ib@gmail.com>
2024-02-03 18:44:50 -0600

    seed configuration with postgre

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 2. Crear una copia del ```.env.template``` y renombrarlo a ```.env``` y cambiar las variables de entorno.
 3. Instalar dependencias con `npm install` o `yarn`.
 4. Levantar la bade de datos `docker compose -f docker-compose.yml up -d`
-5. Ejecutar la aplicación con `npm start` o `yarn start`.
+5. Correr las migraciones de Prisma ```yarn migrate-dev```
+6. Ejecutar seed `yarn seed`
+7. Ejecutar la aplicación con `npm run dev` o `yarn dev`.
 
 - La aplicación se ejecutará en [http://localhost:3000]
---
+++ database/seed-sql.ts
@@ -0,0 +1,55 @@
+import { initialData } from './seed-data'
+import prisma from '../lib/prisma'
+
+async function main() {
+  // 1. Borrar registros previos
+  await Promise.all([
+    await prisma.productImage.deleteMany(),
+    await prisma.product.deleteMany(),
+    await prisma.category.deleteMany(),
+  ])
+
+  const { categories, products } = initialData
+
+  //   Categorías
+  const categoriesData = categories.map((name) => ({ name }))
+  await prisma.category.createMany({
+    data: categoriesData,
+  })
+
+  const categoriesDB = await prisma.category.findMany()
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id
+    return map
+  }, {} as Record<string, string>) //<string=shirt, string=category>
+
+  // Productos
+
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    })
+
+    // Images
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }))
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    })
+  })
+
+  console.log('Se ha ejecutado correctamente')
+}
+
+;(() => {
+  if (process.env.NODE_ENV === 'production') return
+  main()
+})()
